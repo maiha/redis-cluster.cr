5a65b3548662bc9b7afcae92c663e80e631ef2d5
maiha <maiha@wota.jp>
2016-08-03 00:18:16 +0900

    command: Hashes

--- spec/cluster/commands_spec.cr
+++ spec/cluster/commands_spec.cr
@@ -4,7 +4,7 @@ describe Redis::Cluster::Commands do
   info = load_cluster_info("nodes/m1-6379.nodes")
 
   describe "#get" do
-    it "connect to redis" do
+    it "connect to proper node" do
       cluster = Redis::Cluster.new(info)
       cluster.get("3194")
       cluster.close
@@ -12,7 +12,7 @@ describe Redis::Cluster::Commands do
   end
 
   describe "#set" do
-    it "connect to redis" do
+    it "connect to proper node" do
       cluster = Redis::Cluster.new(info)
       value = rand(10000).to_s
       cluster.set("3194", value)
@@ -33,6 +33,32 @@ describe Redis::Cluster::Commands do
     it "shows given info" do
       cluster = Redis::Cluster.new(info)
       cluster.info("v").size.should eq(1)
+      cluster.close
+    end
+  end
+
+  describe "#hset" do
+    it "connect to proper node" do
+      cluster = Redis::Cluster.new(info)
+      cluster.hset("myhash1", "field1", "Hello")
+      cluster.hset("myhash1", "field2", "World")
+      cluster.close
+    end
+  end
+
+  describe "#hget" do
+    it "connect to proper node" do
+      cluster = Redis::Cluster.new(info)
+      cluster.hget("myhash1", "field1").should eq("Hello")
+      cluster.hget("myhash1", "field2").should eq("World")
+      cluster.close
+    end
+  end
+
+  describe "#hgetall" do
+    it "connect to proper node" do
+      cluster = Redis::Cluster.new(info)
+      cluster.hgetall("myhash1").should eq(["field1", "Hello", "field2", "World"])
       cluster.close
     end
   end
--- src/cluster/commands/proxy.cr
+++ src/cluster/commands/proxy.cr
@@ -1,4 +1,10 @@
 module Redis::Cluster::Commands
+  # Strings
   proxy get, key
   proxy set, key, val
+
+  # Hashes
+  proxy hget, key, field
+  proxy hset, key, field, val
+  proxy hgetall, key
 end
