17c286ac6d49eac3a2f82947346b4533d6438df2
maiha <maiha@wota.jp>
2016-08-29 22:49:41 +0900

    Redis::Cluster.connect creates a clustered or standard connection

--- src/cluster/aliases.cr
+++ src/cluster/aliases.cr
@@ -1,3 +1,4 @@
 module Redis::Cluster
   alias Counts = Hash(NodeInfo, Int64)
+  alias Connection = ::Redis | ::Redis::Cluster::Client
 end
--- src/cluster/new.cr
+++ src/cluster/new.cr
@@ -8,4 +8,22 @@ module Redis::Cluster
     bootstrap = info.nodes.map(&.addr).join(",")
     Client.new(bootstrap, password).tap(&.cluster_info = info)
   end
+
+  # Return a Cluster Connection or Standard Connection
+  def self.connect(host : String, port : Int32, password : String? = nil) : Connection
+    redis = ::Redis.new(host, port, password: password)
+
+    begin
+      redis.command(["cluster", "myid"])
+    rescue e : Redis::Error
+      if /This instance has cluster support disabled/ === e.message
+        return redis
+      else
+        # Just raise it because it would be a connection problem like AUTH error.
+        raise e
+      end
+    end
+
+    return ::Redis::Cluster.new("#{host}:#{port}", password: password)
+  end
 end
