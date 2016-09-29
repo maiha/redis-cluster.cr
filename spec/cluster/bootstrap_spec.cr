82013f71b80960039872376ff87fc90c7d2f542f
maiha <maiha@wota.jp>
2016-09-29 19:04:21 +0900

    Bootstrap#sock

--- spec/cluster/bootstrap_spec.cr
+++ spec/cluster/bootstrap_spec.cr
@@ -1,7 +1,7 @@
 require "./spec_helper"
 
-private def bootstrap(host, port, pass = nil)
-  Redis::Cluster::Bootstrap.new(host, port, pass)
+private def bootstrap(host = nil, port = nil, sock = nil, pass = nil)
+  Redis::Cluster::Bootstrap.new(host, port, sock, pass)
 end
 
 private def parse(string) : Redis::Cluster::Bootstrap
@@ -36,29 +36,37 @@ describe Redis::Cluster::Bootstrap do
     end
 
     it "should build default setting from empty string" do
-      parse("").should eq(bootstrap("127.0.0.1", 6379, nil))
+      parse("").should eq(bootstrap("127.0.0.1", 6379))
     end
 
     it "should accept uri with protocol" do
-      parse("redis://a@host:1").should eq(bootstrap("host"     , 1   , "a"))
-      parse("redis://a@host:1").should eq(bootstrap("host"     , 1   , "a"))
-      parse("redis://a@host"  ).should eq(bootstrap("host"     , 6379, "a"))
-      parse("redis://a@:1"    ).should eq(bootstrap("127.0.0.1", 1   , "a"))
-      parse("redis://host:1"  ).should eq(bootstrap("host"     , 1   , nil))
-      parse("redis://host"    ).should eq(bootstrap("host"     , 6379, nil))
-      parse("redis://a@"      ).should eq(bootstrap("127.0.0.1", 6379, "a"))
-      parse("redis://:1"      ).should eq(bootstrap("127.0.0.1", 1   , nil))
+      # host, port
+      parse("redis://a@host:1").should eq(bootstrap("host"     , 1   , pass: "a"))
+      parse("redis://a@host:1").should eq(bootstrap("host"     , 1   , pass: "a"))
+      parse("redis://a@host"  ).should eq(bootstrap("host"     , 6379, pass: "a"))
+      parse("redis://a@:1"    ).should eq(bootstrap("127.0.0.1", 1   , pass: "a"))
+      parse("redis://host:1"  ).should eq(bootstrap("host"     , 1   , pass: nil))
+      parse("redis://host"    ).should eq(bootstrap("host"     , 6379, pass: nil))
+      parse("redis://a@"      ).should eq(bootstrap("127.0.0.1", 6379, pass: "a"))
+      parse("redis://:1"      ).should eq(bootstrap("127.0.0.1", 1   , pass: nil))
+      # sock
+      parse("redis:///tmp/s"  ).should eq(bootstrap(sock: "/tmp/s"))
+      parse("redis://a@/tmp/s").should eq(bootstrap(sock: "/tmp/s", pass: "a"))
     end
 
     it "should accept uri without protocol" do
-      parse("a@host:1").should eq(bootstrap("host"     , 1   , "a"))
-      parse("a@host:1").should eq(bootstrap("host"     , 1   , "a"))
-      parse("a@host"  ).should eq(bootstrap("host"     , 6379, "a"))
-      parse("a@:1"    ).should eq(bootstrap("127.0.0.1", 1   , "a"))
-      parse("host:1"  ).should eq(bootstrap("host"     , 1   , nil))
-      parse("host"    ).should eq(bootstrap("host"     , 6379, nil))
-      parse("a@"      ).should eq(bootstrap("127.0.0.1", 6379, "a"))
-      parse(":1"      ).should eq(bootstrap("127.0.0.1", 1   , nil))
+      # host, port
+      parse("a@host:1").should eq(bootstrap("host"     , 1   , pass: "a"))
+      parse("a@host:1").should eq(bootstrap("host"     , 1   , pass: "a"))
+      parse("a@host"  ).should eq(bootstrap("host"     , 6379, pass: "a"))
+      parse("a@:1"    ).should eq(bootstrap("127.0.0.1", 1   , pass: "a"))
+      parse("host:1"  ).should eq(bootstrap("host"     , 1   , pass: nil))
+      parse("host"    ).should eq(bootstrap("host"     , 6379, pass: nil))
+      parse("a@"      ).should eq(bootstrap("127.0.0.1", 6379, pass: "a"))
+      parse(":1"      ).should eq(bootstrap("127.0.0.1", 1   , pass: nil))
+      # sock
+      parse("/tmp/s"  ).should eq(bootstrap(sock: "/tmp/s"))
+      parse("a@/tmp/s").should eq(bootstrap(sock: "/tmp/s", pass: "a"))
     end
 
     it "should raise when port part is not number nor positive" do
--- src/cluster/bootstrap.cr
+++ src/cluster/bootstrap.cr
@@ -2,9 +2,30 @@ require "uri"
 
 module Redis::Cluster
   record Bootstrap,
-    host : String,
-    port : Int32,
-    pass : String? do
+    host : String? = nil,
+    port : Int32?  = nil,
+    sock : String? = nil,
+    pass : String? = nil do
+
+    def host
+      @host || "127.0.0.1"
+    end
+         
+    def port
+      @port || 6379
+    end
+         
+    def sock?
+      !! @sock
+    end
+         
+    def copy(host : String? = nil, port : Int32? = nil, pass : String? = nil)
+      Bootstrap.new(host: host||@host, port: port||@port, pass: pass||@pass)
+    end
+
+    def redis
+      Redis.new(host: host, port: port, unixsocket: @sock, password: @pass)
+    end
 
     def self.zero
       new(host: Addr::DEFAULT_HOST, port: Addr::DEFAULT_PORT, pass: nil)
@@ -21,19 +42,14 @@ module Redis::Cluster
       end
         
       uri = URI.parse(s)
+      if uri.path && uri.host.nil? && uri.port.nil?
+        return new(sock: uri.path, pass: uri.user)
+      end
       if uri.port && uri.port.not_nil! <= 0
         raise "invalid port for Bootstrap: `#{uri.port}`"
       end
       host = uri.host.to_s.empty? ? nil : uri.host
       zero.copy(host: host, port: uri.port, pass: uri.user)
-    end
-         
-    def copy(host : String? = nil, port : Int32? = nil, pass : String? = nil)
-      Bootstrap.new(host: host||@host, port: port||@port, pass: pass||@pass)
-    end
-
-    def redis
-      Redis.new(host: @host, port: @port, password: @pass)
     end
   end
 end
